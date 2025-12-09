1dab814580aed48ef92f552abe38421d44cb2515
Lotos-Radricas <146979695+Lotos-Radricas@users.noreply.github.com>
2025-12-09 17:00:34 +0500

    16_6_Задание4_Статус сети

---
+++ 16_6/Задание4/script.js
@@ -0,0 +1,230 @@
+function sanitize(html) {
+  const el = document.createElement("div");
+  el.innerHTML = html;
+  return el.textContent;
+}
+
+let user;
+
+function setUser(userData) {
+  user = userData;
+}
+
+function getUser() {
+  return user;
+}
+
+async function getFilms() {
+  try {
+    const user = getUser();
+    const response = await fetch("https://sb-film.skillbox.cc/films", {
+      headers: {
+        email: user?.email
+      }
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      handleErrorResponse(data);
+    }
+
+    return data;
+  } catch (error) {
+    handleError(error);
+    return [];
+  }
+}
+
+function handleErrorResponse(data) {
+  const isNeedAuth = data.errors.some(error => error.location === "headers" && error.param === "email");
+
+  if (isNeedAuth) {
+    const err = new Error("Некорректный email");
+    err.name = "AuthError";
+    throw err;
+  }
+}
+
+function handleError(error) {
+  if (error.name === "AuthError") {
+    throw error;
+  }
+  console.error(error);
+}
+
+function renderTopBar(user) {
+  const el = document.createElement("div");
+  el.classList.add("topbar");
+
+  el.innerHTML = `
+    <span class="topbar-logo">Фильмотека</span>
+    <div class="topbar-user user">
+      <div class="user-name">${sanitize(user.name)}</div>
+      <div class="user-email">${sanitize(user.email)}</div>
+    </div>
+  `;
+
+  return el;
+}
+
+function renderFilms(films) {
+  const el = document.createElement("div");
+  el.classList.add("films");
+
+  if (films.length === 0) {
+    el.innerText = "Cписок фильмов пока пуст";
+    return el;
+  }
+
+  films.forEach((film) => {
+    const filmEl = document.createElement("div");
+    filmEl.classList.add("films-card");
+    filmEl.dataset.watched = film.isWatched;
+
+    filmEl.textContent = `${film.title} (${film.releaseYear})`;
+
+    el.append(filmEl);
+  });
+
+  return el;
+}
+
+function renderGlobalError(message) {
+  const el = document.createElement("div");
+
+  el.innerHTML = `
+    <div class="error">
+      <div class="error-title">Упс... Возникла ошибка</div>
+      <div class="error-message">${sanitize(message)}</div>
+    </error>
+  `;
+
+  return el;
+}
+
+function renderAuthForm(props) {
+  const form = document.createElement("form");
+  form.classList.add("authForm")
+
+  form.innerHTML = `
+    <label for="name">Ваше имя</label>
+    <input id="name" type="text" name="name" required="true" placeholder="Василий" />
+    <label for="email">Эл. почта</label>
+    <input id="email" type="text" name="email" required="true" placeholder="example@mail.com" />
+    <button class="authForm-submit"type="submit">Войти</button>
+  `;
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formProps = Object.fromEntries(formData);
+
+    props.onSubmit(formProps);
+  });
+
+  return form;
+}
+
+function initAuth() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  app.append(renderAuthForm({
+    onSubmit: (user) => {
+      setUser(user);
+      initApp();
+    }
+  }));
+}
+
+async function initApp() {
+  const app = document.getElementById("app");
+  app.innerHTML = "";
+
+  try {
+    const user = getUser();
+    if (!user) {
+      initAuth();
+      return;
+    }
+    const films = await getFilms();
+    app.append(renderTopBar(user));
+    app.append(renderFilms(films));
+  } catch (error) {
+    console.error(error);
+
+    if (error.name === "AuthError") {
+      initAuth();
+      return;
+    }
+
+    app.append(renderGlobalError(error.message));
+  }
+}
+
+initApp();
+
+// Создаем контейнер для уведомлений
+const networkStatusNotification = document.createElement("div");
+networkStatusNotification.id = "network-status";
+networkStatusNotification.style.position = "fixed";
+networkStatusNotification.style.bottom = "200px";
+networkStatusNotification.style.left = "50%";
+networkStatusNotification.style.marginLeft = "-100px";
+networkStatusNotification.style.fontFamily = 'Arial, sans-serif';
+networkStatusNotification.style.fontSize = '14px';
+networkStatusNotification.style.width = "200px";
+networkStatusNotification.style.zIndex = "9999";
+networkStatusNotification.style.textAlign = "center";
+networkStatusNotification.style.padding = "10px";
+networkStatusNotification.style.color = "#fff";
+networkStatusNotification.style.backgroundColor = "#f8bc06ff";
+networkStatusNotification.style.borderRadius = '15px';
+networkStatusNotification.style.display = "none";
+
+document.body.appendChild(networkStatusNotification);
+
+// Функция отображения сообщения
+function showNetworkMessage(message, colorMsg) {
+  networkStatusNotification.innerText = message;
+  networkStatusNotification.style.backgroundColor = colorMsg;
+  networkStatusNotification.style.display = "block";
+}
+
+// Функция скрытия уведомления
+function hideNetworkMessage() {
+  networkStatusNotification.style.display = "none";
+}
+
+// Проверка соединения
+async function checkNetworkStatus() {
+
+  try {
+    const startTime = Date.now();
+    await fetch("https://sb-film.skillbox.cc/ping", {
+      method: "POST",
+
+    });
+    const duration = Date.now() - startTime;
+
+    if (duration > 500) {
+      // Медленное соединение
+      showNetworkMessage("Медленное соединение", "#ff9800");
+ 
+    } else {
+      // Соединение в порядке
+      hideNetworkMessage();
+    }
+  } catch (error) {
+    
+    // Ошибка сети или запрос прерван
+    showNetworkMessage("Неполадки с сетью", "#f90606fe");
+    console.error(error);
+
+  }
+}
+
+// Запускаем опрос каждые 5 секунд
+setInterval(checkNetworkStatus, 5000);
